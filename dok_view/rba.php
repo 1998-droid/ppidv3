9fa1d5c0b60de698c9b13995f07afb08d26e5eec
1998-droid <tiotiotio98@gmail.com>
2022-06-23 14:00:41 +0700

    commit -m

--- dok_view/rba.php
+++ dok_view/rba.php
@@ -3,7 +3,7 @@ include_once "../_header.php";
 ?>
 <div class="container">
 <h4>LKJIP 2021 RSUD MAJENANG</h4>
-<embed type="application/pdf" src="../asset/doc/rba2022.pdf" width="1100" height="500"></embed> 
+<embed type="application/pdf" src="../asset/doc/rab2022.pdf" width="1100" height="500"></embed> 
 </div>
 <?php
 include_once "../_footer.php"
