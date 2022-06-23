8266c574f7094106b5248b8c6790daae28590ac0
1998-droid <tiotiotio98@gmail.com>
2022-06-23 09:24:19 +0700

    commit -m

---
+++ dok_view/lkjip.php
@@ -0,0 +1,10 @@
+<?php
+include_once "../_header.php";
+?>
+<div class="container">
+<h4>LKJIP 2021 RSUD MAJENANG</h4>
+<embed type="application/pdf" src="../asset/doc/lkjip2021.pdf" width="1100" height="500"></embed> 
+</div>
+<?php
+include_once "../_footer.php"
+?>
--- inf_publik/berkala.php
+++ inf_publik/berkala.php
@@ -67,6 +67,12 @@ include_once "../_header.php";
             <li class="list-group-item list-group-action"><a href="#">Standar Prosedur Operasional (SPO) Permohonan Infromasi Publik</a></li>
 </ul>
 
+<h4>7. Data Kinerja</h4>
+<ul class="list-group">
+<li class="list-group-item list-group-action"><a href="../dok_view/lkjip.php">LKJIP 2020</a></li>
+<li class="list-group-item list-group-action"><a href="../dok_view/lkjip.php">LKJIP 2021</a></li>
+</ul>
+
 <nav aria-label="Page navigation example">
   <ul class="pagination">
     <li class="page-item">
